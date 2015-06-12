135e12f4e8de61027846866818dee191dfb06f0e
Rajat Bhageria <rajat1996@gmail.com>
2015-06-11 22:26:48 -0400

    Finished basic version of game!

--- ViewController.swift
+++ ViewController.swift
@@ -14,11 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondCardImageView: UIImageView!
     @IBOutlet weak var playRoundButton: UIButton!
     @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    @IBOutlet weak var playerScore: UILabel!
+    @IBOutlet weak var enemyScore: UILabel!
+    
+    
+    var firstPlayerTotal = 0;
+    var enemyPlayerTotal = 0;
+    var cardArray:[String] = ["ace","card2","card3","card4","card5",
+        "card6","card7","card8","card9","card10","jack","queen","king"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        self.playRoundButton.setTitle("Play", forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,6 +34,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playRoundTapped(sender: UIButton) {
+        var firstRandomNumber: Int = Int(arc4random_uniform(13))
+        var firstRandomCard = cardArray[firstRandomNumber]
+        self.firstCardImageView.image = UIImage(named: firstRandomCard)
+        
+        var secondRandomNumber: Int = Int(arc4random_uniform(13))
+        var secondRandomCard = cardArray[secondRandomNumber]
+        self.secondCardImageView.image = UIImage(named: secondRandomCard)
+
+
+        if (firstRandomNumber > secondRandomNumber){
+            firstPlayerTotal += 1
+            playerScore.text = String(firstPlayerTotal);
+        } else if (firstRandomNumber == secondRandomNumber){
+            
+        } else{
+            enemyPlayerTotal += 1
+            enemyScore.text = String(enemyPlayerTotal);
+
+        }
+    }
 
 }
 
